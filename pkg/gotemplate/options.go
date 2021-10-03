0759fa02b6cc919273d76c4a65a707b488633011
Tobias Brumhard <tobias.brumhard@mail.schwarz>
2021-10-03 17:44:59 +0200

    Add documenation to options

--- pkg/gotemplate/options.go
+++ pkg/gotemplate/options.go
@@ -12,64 +12,66 @@ import (
 // ErrInvalidaPattern indicates that an error occured while matching
 // a value with a pattern.
 // The pattern is included in the error message.
-
 type ErrInvalidPattern struct {
 	Value   string
 	Pattern string
 }
 
 func (e *ErrInvalidPattern) Error() string {
-	return fmt.Sprint("%s: invalid pattern (expected %s)", e.Value, e.Pattern)
+	return fmt.Sprintf("%s: invalid pattern (expected %s)", e.Value, e.Pattern)
 }
 
-// TODO: remove interface?
-type Option interface {
-	Name() string
-	Description(currentValues OptionValues) string
-	Default(currentValues OptionValues) interface{}
-	ShouldDisplay(currentValues OptionValues) bool
-	// validate receives a value that should be set for the option and validates it
-	Validate(value interface{}) error
-	// TODO: should the value be kept in the struct and reused here instead of passing it?
-	PostHook(value interface{}) error
-}
-
-var _ Option = &SomeOption{}
-
+// Validator is a single method interface that validates that a given value is valid.
+// If any error happens during validation or if the value is not valid an error will be returned.
 type Validator interface {
 	Validate(value interface{}) error
 }
 
+// ValidatorFunc is a function implementing the Validator interface.
 type ValidatorFunc func(value interface{}) error
 
 func (f ValidatorFunc) Validate(value interface{}) error {
 	return f(value)
 }
 
-type SomeOption struct {
-	name          string
-	description   StringValuer
-	hook          func(interface{}) error
-	defaultValue  Valuer
-	validator     Validator
+// Option is a struct containing all needed configuration for options to customize the template.
+type Option struct {
+	// name is the name of the option that will be used to reference it and also that will be shown on the cli.
+	name string
+	// description is the description of the option that should be shown.
+	// It's a StringValuer since it could depend on some earlier input.
+	description StringValuer
+	// defaultValue is the default value of the option.
+	// It's a Valuer since it could be depend on earlier inputs or some http call.
+	defaultValue Valuer
+	// validator is used to validate an input value if it can be used as the value for this option.
+	// If it is not set it will by default by valid.
+	validator Validator
+	// shouldDisplay decides whether the option is shown when the values are loaded interactively.
+	// In most cases this is used to ensure options are only shown if needed values have been supplied earlier.
+	// If it is not set it will by default be shown.
 	shouldDisplay BoolValuer
+	// postHook is some function that will be executed after all options are loaded.
+	// This can for example be used to remove files from the created project folder or initialize tools based on inputs.
+	postHook func(interface{}) error
 }
 
-func (s *SomeOption) Name() string {
+func (s *Option) Name() string {
 	return s.name
 }
 
-func (s *SomeOption) Description(currentValues OptionValues) string {
+func (s *Option) Description(currentValues OptionValues) string {
 	return s.description.Value(currentValues)
 }
 
-// Value either returns the default value (possibly calculated with currentValues)
-// or the actually set value
-func (s *SomeOption) Default(currentValues OptionValues) interface{} {
+// Default either returns the default value (possibly calculated with currentValues).
+func (s *Option) Default(currentValues OptionValues) interface{} {
 	return s.defaultValue.Value(currentValues)
 }
 
-func (s *SomeOption) ShouldDisplay(currentValues OptionValues) bool {
+// ShouldDisplay returns a bool value indicating whether the option should be shown or not.
+// If shouldDisplay variable is not set on the option true is returned.
+func (s *Option) ShouldDisplay(currentValues OptionValues) bool {
 	if s.shouldDisplay != nil {
 		return s.shouldDisplay.Value(currentValues)
 	}
@@ -77,7 +79,8 @@ func (s *SomeOption) ShouldDisplay(currentValues OptionValues) bool {
 	return true
 }
 
-func (s *SomeOption) Validate(value interface{}) error {
+// Validate validates the value if a validator is specified.
+func (s *Option) Validate(value interface{}) error {
 	if s.validator != nil {
 		return s.validator.Validate(value)
 	}
@@ -85,34 +88,36 @@ func (s *SomeOption) Validate(value interface{}) error {
 	return nil
 }
 
-func (s *SomeOption) PostHook(v interface{}) error {
-	if s.hook != nil {
-		return s.hook(v)
+// PostHook executes the registered postHook if there is any.
+func (s *Option) PostHook(v interface{}) error {
+	if s.postHook != nil {
+		return s.postHook(v)
 	}
 
 	return nil
 }
 
-type Regex struct {
-	Pattern     regexp.Regexp
-	Description string
-}
-
+// Category is used to wrap multiple extensions into one organizational unit.
+// This is to reduce the amount of required user input if certain categories if extensions
+// can be skipped as a category instead of needing to skip all one by one.
 type Category struct {
 	Name    string
 	Options []Option
 }
 
-// would be easier to make the Options hardcoded or maps to access them later one typesafe
-// but there would be no option to read in the things dynamically without reflection
-// -> either dynamic during reading the values
-// -> or while evaluating them and dependencies...
-// can't be maps since the order of looping through maps is random
+// Options is the main struct wrapping the configuration
+// for all allowed parameters and extensions.
+// Slices are used instead of maps since the iteration order of maps is undefined/random.
+// which could lead to confusion.
 type Options struct {
 	Base       []Option
 	Extensions []Category
 }
 
+// OptionValues is a struct mirroring the structure of Options but using maps.
+// Instead of the whole option only the set value of the option is kept.
+// This makes looking up already supplied option values easier than it would
+// be in the Options struct.
 type OptionValues struct {
 	Base       OptionNameToValue
 	Extensions map[string]OptionNameToValue
@@ -120,15 +125,16 @@ type OptionValues struct {
 
 type OptionNameToValue map[string]interface{}
 
+// NewOptions returns all of go/template's options.
 func NewOptions(githubTagLister repos.GithubTagLister) *Options {
 	return &Options{
 		Base: []Option{
-			&SomeOption{
+			{
 				name:         "projectName",
 				defaultValue: StaticValue("Awesome Project"),
 				description:  StringValue("Name of the project"),
 			},
-			&SomeOption{
+			{
 				name: "projectSlug",
 				defaultValue: DynamicValue(func(ov OptionValues) interface{} {
 					projectName := ov.Base["projectName"].(string)
@@ -137,18 +143,18 @@ func NewOptions(githubTagLister repos.GithubTagLister) *Options {
 				description: StringValue("Technical name of the project for folders and names. This will also be used as output directory."),
 				validator:   RegexValidator(`^[a-z1-9]+(-[a-z1-9]+)*$`, "only lowercase letters and dashes"),
 			},
-			&SomeOption{
+			{
 				name:         "projectDescription",
 				defaultValue: StaticValue("The awesome project provides awesome features to awesome people."),
 				description:  StringValue("Description of the project used in the README."),
 			},
-			&SomeOption{
+			{
 				name:         "appName",
 				defaultValue: StaticValue("awesomecli"),
 				description:  StringValue("The name of the binary that you want to create. Could be the same your `project_slug` but since Go supports multiple apps in one repo it could also be sth. else. For example if your project is for some API there could be one app for the server and one CLI client."),
 				validator:    RegexValidator(`^[a-z]+$`, "only lowercase letters"),
 			},
-			&SomeOption{
+			{
 				name: "moduleName",
 				defaultValue: DynamicValue(func(vals OptionValues) interface{} {
 					projectSlug := vals.Base["projectSlug"].(string)
@@ -157,7 +163,7 @@ func NewOptions(githubTagLister repos.GithubTagLister) *Options {
 				description: StringValue("The name of the Go module defined in the `go.mod` file. This is used if you want to `go get` the module. Please be aware that this depends on your version control system. The default points to `github.com` but for devops for example it would look sth. like this `dev.azure.com/org/project/repo.git`"),
 				validator:   RegexValidator(`^[\S]+$`, "no whitespaces"),
 			},
-			&SomeOption{
+			{
 				name: "golangciVersion",
 				defaultValue: DynamicValue(func(_ OptionValues) interface{} {
 					latestTag, err := repos.LatestGithubReleaseTag(githubTagLister, "golangci", "golangci-lint")
@@ -178,7 +184,7 @@ func NewOptions(githubTagLister repos.GithubTagLister) *Options {
 			{
 				Name: "cicd",
 				Options: []Option{
-					&SomeOption{
+					{
 						name:         "pipeline",
 						defaultValue: StaticValue(1),
 						validator:    RegexValidator(`^[1-2]$`, "number between 1-2"),
@@ -186,7 +192,7 @@ func NewOptions(githubTagLister repos.GithubTagLister) *Options {
 	Options:
 		1. Github
 		2. Azure Devops`),
-						hook: func(v interface{}) error {
+						postHook: func(v interface{}) error {
 							val := v.(int)
 							dirs := []string{".github", ".azuredevops"}
 							switch val {
@@ -203,10 +209,10 @@ func NewOptions(githubTagLister repos.GithubTagLister) *Options {
 			{
 				Name: "grpc",
 				Options: []Option{
-					&SomeOption{
+					{
 						name:         "base",
 						defaultValue: StaticValue(false),
-						hook: func(v interface{}) error {
+						postHook: func(v interface{}) error {
 							set := v.(bool)
 							files := []string{"api/proto", "tools.go", "buf.gen.yaml", "buf.yaml", "api/openapi.v1.yaml"}
 
@@ -216,7 +222,7 @@ func NewOptions(githubTagLister repos.GithubTagLister) *Options {
 							return removeAllBut(files, "api/openapi.v1.yaml")
 						},
 					},
-					&SomeOption{
+					{
 						name:         "grpcGateway",
 						defaultValue: StaticValue(false),
 						shouldDisplay: DynamicBoolValue(func(vals OptionValues) bool {
@@ -229,7 +235,7 @@ func NewOptions(githubTagLister repos.GithubTagLister) *Options {
 	}
 }
 
-// removeAllBut removes all files in the toRemove slice except for the exception
+// removeAllBut removes all files in the toRemove slice except for the exception.
 func removeAllBut(toRemove []string, exception string) error {
 	for _, item := range toRemove {
 		if item == exception {
@@ -244,6 +250,8 @@ func removeAllBut(toRemove []string, exception string) error {
 	return nil
 }
 
+// RegexValidator returns a ValidatorFunc to validate a given value against a regex pattern.
+// If the pattern doesn't match a ErrInvalidPattern is returned with a description on what the pattern means.
 func RegexValidator(pattern, description string) ValidatorFunc {
 	return func(value interface{}) error {
 		str := value.(string)
