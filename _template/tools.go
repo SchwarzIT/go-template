b8d7951681c88db7317fd51d5e15f4f6398c3e98
Steffen Exler <Steffen.Exler@pm.me>
2023-02-10 16:20:19 +0100

    Set version control of golangci into go.mod
    Signed-off-by: Steffen Exler <Steffen.Exler@pm.me>

--- _template/tools.go
+++ _template/tools.go
@@ -3,14 +3,22 @@
 package main
 
 import (
+	// golangci linter
+	// https://golangci-lint.run
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	{{- if .Extensions.grpc.base }}
+
+	// gRPC
 	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/bufbuild/buf/cmd/protoc-gen-buf-breaking"
 	_ "github.com/bufbuild/buf/cmd/protoc-gen-buf-lint"
 	_ "github.com/envoyproxy/protoc-gen-validate"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go" {{ end -}}
 	{{- if .Extensions.grpc.grpcGateway }}
+
+	// gRPC Gateway
+	// https://github.com/grpc-ecosystem/grpc-gateway
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
-	_ "github.com/google/gnostic/cmd/protoc-gen-openapi"
-	{{- end }}
+	_ "github.com/google/gnostic/cmd/protoc-gen-openapi" {{- end }}
 )
---
+++ tools.go
@@ -0,0 +1,9 @@
+//go:build tools
+
+package main
+
+import (
+	// golangci linter
+	// https://golangci-lint.run
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+)
