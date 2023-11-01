a3615f6f92688e41e74d9f0e8a381de1cb7508c7
zalgonoise <mariozalgo@gmail.com>
2023-11-01 18:20:07 +0100

    go-template/_template/cmd/app: swapped zap logger with log/slog

--- _template/cmd/{{.Base.appName}}/main.go
+++ _template/cmd/{{.Base.appName}}/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"os"
-
-	"{{.Base.moduleName}}/internal/log"
-	"go.uber.org/zap"
 
 	// This controls the maxprocs environment variable in container runtimes.
 	// see https://martin.baillie.id/wrote/gotchas-in-the-go-network-packages-defaults/#bonus-gomaxprocs-containers-and-the-cfs
 	_ "go.uber.org/automaxprocs"
+
+	"{{.Base.moduleName}}/internal/log"
 )
 
 func main() {
@@ -20,16 +21,14 @@ func main() {
 }
 
 func run() error {
-	logger, err := log.NewAtLevel(os.Getenv("LOG_LEVEL"))
+	logger, err := log.New(os.Getenv("LOG_LEVEL"))
 	if err != nil {
 		return err
 	}
 
-	defer func() {
-		err = logger.Sync()
-	}()
+	ctx := context.Background()
 
-	logger.Info("Hello world!", zap.String("location", "world"))
+	logger.InfoContext(ctx, "Hello world!", slog.String("location", "world"))
 
 	return err
 }
