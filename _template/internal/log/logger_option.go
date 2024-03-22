e057254cd650acb19762d2c6872ed837d0d56f99
Mario_Andre.Pinto_Ramos_Salgado <mario_andre.pinto_ramos_salgado@mail.schwarz>
2024-03-22 12:50:42 +0100

    fixup! go-template/_template/internal/log: swapped zap logger with log/slog; added trace context handler; added gRPC interceptor logger

--- _template/internal/log/logger_option.go
+++ _template/internal/log/logger_option.go
@@ -1,1 +1,59 @@
 package log
+
+import (
+	"io"
+	"log/slog"
+)
+
+type Config struct {
+	handler   slog.Handler
+	writer    io.Writer
+	addSource bool
+	level     string
+}
+
+type Option interface {
+	apply(*Config)
+}
+
+type OptionFunc func(*Config)
+
+func (o OptionFunc) apply(c *Config) {
+	o(c)
+}
+
+func WithHandler(handler slog.Handler) OptionFunc {
+	if handler == nil {
+		return func(config *Config) {}
+	}
+
+	return func(config *Config) {
+		config.handler = handler
+	}
+}
+
+func WithWriter(writer io.Writer) OptionFunc {
+	if writer == nil {
+		return func(config *Config) {}
+	}
+
+	return func(config *Config) {
+		config.writer = writer
+	}
+}
+
+func WithLevel(level string) OptionFunc {
+	if level == "" {
+		return func(config *Config) {}
+	}
+
+	return func(config *Config) {
+		config.level = level
+	}
+}
+
+func WithSource() OptionFunc {
+	return func(config *Config) {
+		config.addSource = true
+	}
+}
