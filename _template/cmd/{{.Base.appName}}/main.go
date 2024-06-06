c687b430467f3b38d2fed292f853a34d455c2de6
Mario_Andre.Pinto_Ramos_Salgado <mario_andre.pinto_ramos_salgado@mail.schwarz>
2024-06-06 14:48:39 +0200

    fixup! go-template/_template/cmd/app: swapped zap logger with log/slog

--- _template/cmd/{{.Base.appName}}/main.go
+++ _template/cmd/{{.Base.appName}}/main.go
@@ -8,7 +8,7 @@ import (
 
 	// This controls the maxprocs environment variable in container runtimes.
 	// see https://martin.baillie.id/wrote/gotchas-in-the-go-network-packages-defaults/#bonus-gomaxprocs-containers-and-the-cfs
-	_ "go.uber.org/automaxprocs"
+	"go.uber.org/automaxprocs/maxprocs"
 
 	"{{.Base.moduleName}}/internal/log"
 )
